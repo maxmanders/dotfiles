adb61a46123697e09dc8e309e6690fb31dd83a95
Max Manders <max@maxmanders.co.uk>
2021-01-24 12:42:53 +0000

    Don't hardcode paths

--- .zshrc.d/.fzf.zsh
+++ .zshrc.d/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/max/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/max/.fzf/bin"
+if [[ ! "$PATH" == "*${HOME}/.fzf/bin*" ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/max/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/max/.fzf/shell/key-bindings.zsh"
+source "${HOME}/.fzf/shell/key-bindings.zsh"
