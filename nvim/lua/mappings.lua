f8fa7f4b79da338bcc6e79a17ce126b0fbf627cb
Max Manders <max@maxmanders.co.uk>
2024-09-20 17:04:36 +0100

    Fix tabs

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,15 +6,15 @@ local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
 map("i", "<C-l>", function()
-	vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
 end, {
-		desc = "Copilot Accept",
-		replace_keycodes = true,
-		nowait=true,
-		silent=true,
-		expr=true,
-		noremap=true,
-	}
+  desc = "Copilot Accept",
+  replace_keycodes = true,
+  nowait=true,
+  silent=true,
+  expr=true,
+  noremap=true,
+  }
 )
 
 map("n", "<leader>gg", "<cmd> LazyGit<CR>", { desc = "Toggle LazyGit" })
