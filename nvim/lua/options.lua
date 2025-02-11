d045da4e81befb03f0b7e9b99fbf229f49fc0544
Max Manders <max@maxmanders.co.uk>
2025-02-11 11:45:12 +0000

    feat: set up Go plugins

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,3 +8,13 @@ vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 vim.wo.relativenumber = true
 vim.opt.hls = false
 vim.opt.mouse = ""
+
+-- autoformat Go files on save
+local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
