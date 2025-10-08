43b470585615d19a74b70efe2a0e24eebaad2f79
Max Manders <max@maxmanders.co.uk>
2025-10-08 09:01:59 +0100

    feat: update nvchad config

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,6 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
 local servers = { "bashls", "html", "cssls", "ts_ls", "terraformls", "clangd", "pyre", "pyright", "gopls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+vim.lsp.enable(servers)
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+-- read :h vim.lsp.config for changing options of lsp servers 
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,6 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "<C-l>", function()
   vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
