7f2c0cb56fb5ee5eb4e1bb53bc17174a5c204186
Max Manders <max@maxmanders.co.uk>
2025-10-10 14:07:50 +0100

    feat: install Helm language server

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "bashls", "html", "cssls", "ts_ls", "terraformls", "clangd", "pyre", "pyright", "gopls" }
+local servers = { "bashls", "helm-ls", "html", "cssls", "ts_ls", "terraformls", "clangd", "pyre", "pyright", "gopls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
