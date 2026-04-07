315a013807d2200f9262bb7edd6c00bd949bd12c
Max Manders <max@maxmanders.co.uk>
2026-04-07 14:58:28 +0100

    feat: Neovim 0.12 - treesitter fixes
    The maintainer of nvim-treesitter moved from `master` to `main` with a
    complete rewrite for Neovim 0.12. TL;DR we need to make some changes.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,6 +13,21 @@ return {
     end,
   },
 
+{
+  'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+
   {
     -- Install markdown preview, use npx if available.
     "iamcco/markdown-preview.nvim",
