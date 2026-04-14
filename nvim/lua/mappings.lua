9ace72412af86d1512ad54c0891e055a21053084
Max Manders <max@maxmanders.co.uk>
2026-04-14 14:18:48 +0100

    feat: add keymaps for bubbling lines

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -24,3 +24,9 @@ map("n", "<leader>gg", "<cmd> LazyGit<CR>", { desc = "Toggle LazyGit" })
 map("v", ">", ">gv", { desc = "Indent text" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Bubble lines up/down
+map("n", "]e", "<cmd>m .+1<CR>==",  { desc = "Move line down" })
+map("n", "[e", "<cmd>m .-2<CR>==",  { desc = "Move line up" })
+map("v", "]e", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "[e", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
