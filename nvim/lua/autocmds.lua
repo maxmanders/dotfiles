abe5cfab606b71970c56bc2e5f4b1ecfa6604846
Max Manders <max@maxmanders.co.uk>
2026-06-04 17:31:02 +0100

    feat: retain swap files for temporary buffers

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,5 +1,18 @@
 require "nvchad.autocmds"
 
+-- Re-enable swap files for temp directory files (e.g. buffers opened by gh pr create, git commit, etc.)
+-- so that editor crashes don't lose the PR body or commit message.
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  callback = function(args)
+    local file = args.file
+    if file == "" then return end
+    local tmpdir = (os.getenv("TMPDIR") or "/tmp"):gsub("/$", "")
+    if file:find("^/tmp/", 1, true) or file:find("^" .. vim.pesc(tmpdir) .. "/", 1) then
+      vim.opt_local.swapfile = true
+    end
+  end,
+})
+
 vim.filetype.add({
   extension = {
     hujson = "jsonc",
